0e135046bd68226888bab3d0eafe0af4e3708823
M Faisal <aapka-github-email@gmail.com>
2026-02-25 20:24:05 +0500

    create a header components

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Home from './pages/Home'
+import Header from './components/Header'
 
 function App() {
     return <BrowserRouter>
+    <Header/>
   <Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="/profile" element={<Profile/>}/>
