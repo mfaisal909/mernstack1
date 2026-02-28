77bdf4bf6cd1331efcb26756a877f603dc1e9c88
M Faisal <aapka-github-email@gmail.com>
2026-02-28 17:36:09 +0500

    delete account

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,6 +6,9 @@ import {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
 } from "../redux/user/userSlice";
 
 export default function Profile() {
@@ -115,7 +118,23 @@ export default function Profile() {
       dispatch(updateUserFailure(error.message));
     }
   };
-
+  const handleDeleteUser=async()=>{
+    try {
+      dispatch(deleteUserStart());
+      const res=await fetch(`/api/user/delete/${currentUser._id}`,{
+        method:'DELETE'
+      });
+      const data=await res.json()
+      if(data.success===false)
+      {
+        dispatch(deleteUserFailure(data.message));
+        return ;
+      }
+      dispatch(deleteUserSuccess(data))
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message))
+    }
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
@@ -181,7 +200,7 @@ export default function Profile() {
       </form>
 
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">
+        <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">
           Delete Account
         </span>
         <span className="text-red-700 cursor-pointer">
