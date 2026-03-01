9d015ad7420c7ec7c284f89fbb6336c8a3385eb8
M Faisal <aapka-github-email@gmail.com>
2026-03-01 10:32:52 +0500

    complete correct imges are upload

--- src/pages/Createlisting.jsx
+++ src/pages/Createlisting.jsx
@@ -1,6 +1,56 @@
-import React from "react";
+import { useState } from "react";
+import { supabase } from "../supabase";
 
 export default function CreateListing() {
+  const [files,setFiles]=useState([])
+  const [imageUrls,setImagesUrls]=useState([])
+  const [uploading,setUploading]=useState(false);
+  const [error,setError]=useState(null)
+  console.log(files)
+  const handleImageSubmit=async(e)=>{
+    if(files.length===0)
+    {
+      setError("Please select at latest 1 images");
+      return ;
+    }
+    if(files.length>6)
+    {
+      setError("Maximum 6 images allowed");
+      return ;
+    }
+    setUploading(true);
+    setError(null)
+      try {
+        const uploadPromises=files.map((file)=>storeImage(file))
+        const urls = await Promise.all(uploadPromises)
+        setImagesUrls(urls)
+        console.log("Uploaded Images:",urls)
+      } catch (err) {
+        console.error(err)
+        setError("Image upload fail (2MB max per image)")
+      }
+      setUploading(false)
+    };
+  
+  const storeImage=async(file)=>{
+    if(file.size>2*1024*1024)
+    {
+      throw new Error("File size must be less than 2MB")
+    }
+    const fileName=`${Date.now()} - ${file.name}`;
+    const {error}=await supabase.storage
+    .from("avatars")
+    .upload(fileName,file);
+
+    if(error)
+    {
+      throw error 
+    }
+    const {data} =supabase.storage
+    .from("avatars")
+    .getPublicUrl(fileName)
+    return data.publicUrl
+  }
   return (
     <main className="p-6 max-w-6xl mx-auto">
       <div className="bg-white shadow-xl rounded-2xl p-8">
@@ -10,7 +60,6 @@ export default function CreateListing() {
 
         <form className="flex flex-col lg:flex-row gap-10">
           
-          {/* LEFT SIDE */}
           <div className="flex flex-col gap-6 flex-1">
             
             <div>
@@ -54,7 +103,6 @@ export default function CreateListing() {
               />
             </div>
 
-            {/* CHECKBOX SECTION */}
             <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
               {["Sell", "Rent", "Parking Spot", "Furnished", "Offer"].map(
                 (item, index) => (
@@ -72,7 +120,7 @@ export default function CreateListing() {
               )}
             </div>
 
-            {/* NUMBER INPUTS */}
+        
             <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
               
               <div>
@@ -128,7 +176,6 @@ export default function CreateListing() {
             </div>
           </div>
 
-          {/* RIGHT SIDE */}
           <div className="flex flex-col flex-1 gap-6">
             
             <div>
@@ -141,6 +188,7 @@ export default function CreateListing() {
 
               <div className="flex gap-4 mt-4">
                 <input
+                  onChange={(e)=>setFiles([...e.target.files])}
                   type="file"
                   accept="image/*"
                   multiple
@@ -148,12 +196,29 @@ export default function CreateListing() {
                 />
                 <button
                   type="button"
+                  onClick={handleImageSubmit}
+                  disabled={uploading}
                   className="px-6 py-3 bg-green-600 text-white rounded-xl hover:bg-green-700 transition duration-200 shadow-md"
                 >
-                  Upload
+                  {uploading?"Uploading...":"Upload"}
                 </button>
               </div>
+               {error && (
+                <p className="text-red-600 mt-2 text-sm">{error}</p>
+              )}
             </div>
+              {imageUrls.length > 0 && (
+              <div className="grid grid-cols-3 gap-4">
+                {imageUrls.map((url, index) => (
+                  <img
+                    key={index}
+                    src={url}
+                    alt="listing"
+                    className="h-24 w-full object-cover rounded-lg"
+                  />
+                ))}
+              </div>
+            )}
 
             <button
               type="submit"
@@ -167,4 +232,4 @@ export default function CreateListing() {
       </div>
     </main>
   );
-}
+}  
